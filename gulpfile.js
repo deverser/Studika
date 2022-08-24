9c48daac699053dc1b3f14198312666fbb33aa4f
Alexander <alexmal74@gmail.com>
2022-08-24 12:04:35 +0500

    Added gitignore, installed gulp for project

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+function defaultTask(cb) {
+	// place code for your default task here
+	cb();
+}
+
+exports.default = defaultTaskfunction defaultTask(cb) {
+	// place code for your default task here
+	cb();
+}
+
+exports.default = defaultTask
